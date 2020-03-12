da72f01514e1733f10486b4ed7c9c7ec837d2f65
Kai <shadowkai0121@gmail.com>
2020-03-12 21:51:06 +0800

    HtmlWebpackPlugin config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
+    // https://github.com/jantimon/html-webpack-plugin/issues/895
+    // 解決 Entrypoint undefined = ..\index.html
+    stats: {
+        children: false,
+    },
     entry: __dirname + '/app/main.js',
     output: {
         path: __dirname + '/build',
         filename: '[name].js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 指定檔案的位置產生 html 文件
+            filename: __dirname + '/index.html',
+        }),
+    ],
 };
